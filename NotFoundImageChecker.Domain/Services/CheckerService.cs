eb9b49272d060d675ddfb6a7091d59a1b9cf2923
Asbjørn Hansen <asser-dk@outlook.com>
2016-04-21 11:04:55 +0200

    extracted file logic into a separate service

--- NotFoundImageChecker.Domain/Services/CheckerService.cs
+++ NotFoundImageChecker.Domain/Services/CheckerService.cs
@@ -3,18 +3,20 @@
     using System;
     using System.IO;
     using System.Linq;
-    using System.Security.Cryptography;
     using SexyFishHorse.NotFoundImageChecker.Domain.Configuration;
 
     public class CheckerService
     {
         private readonly Configuration configuration;
 
+        private readonly FileSystemService fileSystemService;
+
         private readonly FileSystemWatcher watcher;
 
-        public CheckerService(Configuration configuration)
+        public CheckerService(Configuration configuration, FileSystemService fileSystemService)
         {
             this.configuration = configuration;
+            this.fileSystemService = fileSystemService;
 
             watcher = new FileSystemWatcher
             {
@@ -36,18 +38,14 @@
                 throw new DirectoryNotFoundException(string.Format("The directory {0} was not found", folderPath));
             }
 
-            var filePaths = Directory.EnumerateFiles(folderPath);
+            var filePaths = fileSystemService.GetFilePaths();
 
             var filesToDelete = filePaths
-                .Select(x => new { Path = x, Checksum = GenerateChecksum(x) })
+                .Select(x => new { Path = x, Checksum = fileSystemService.GenerateChecksum(x) })
                 .Where(x => configuration.KnownSha256Hashes.Contains(x.Checksum))
                 .Select(x => x.Path);
 
-            foreach (var fileToDelete in filesToDelete)
-            {
-                File.Delete(fileToDelete);
-                Console.WriteLine("Deleted " + fileToDelete);
-            }
+            fileSystemService.DeleteFiles(filesToDelete);
         }
 
         public void StartFolderWatcher()
@@ -56,37 +54,19 @@
             Console.WriteLine("Folder watcher started");
         }
 
-        private string GenerateChecksum(string filePath)
-        {
-            using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-            {
-                var sha = new SHA256Managed();
-                var checksumBytes = sha.ComputeHash(stream);
-
-                return BitConverter.ToString(checksumBytes).Replace("-", string.Empty);
-            }
-        }
-
         private void WatcherOnChanged(object sender, FileSystemEventArgs fileSystemEventArgs)
         {
-            Console.WriteLine("File changed: " + fileSystemEventArgs.FullPath);
+            Console.WriteLine("File changed: {0}", fileSystemEventArgs.Name);
             if (!File.Exists(fileSystemEventArgs.FullPath))
             {
                 return;
             }
 
-            try
-            {
-                var checksum = GenerateChecksum(fileSystemEventArgs.FullPath);
+            var checksum = fileSystemService.GenerateChecksum(fileSystemEventArgs.FullPath);
 
-                if (checksum != null && configuration.KnownSha256Hashes.Contains(checksum))
-                {
-                    File.Delete(fileSystemEventArgs.FullPath);
-                    Console.WriteLine("Deleted " + fileSystemEventArgs.FullPath);
-                }
-            }
-            catch (IOException)
+            if (configuration.KnownSha256Hashes.Contains(checksum))
             {
+                fileSystemService.DeleteFile(fileSystemEventArgs.FullPath);
             }
         }
     }
---
+++ NotFoundImageChecker.Domain/Services/FileSystemService.cs
@@ -0,0 +1,57 @@
+﻿namespace SexyFishHorse.NotFoundImageChecker.Domain.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Security.Cryptography;
+    using SexyFishHorse.NotFoundImageChecker.Domain.Configuration;
+
+    public class FileSystemService
+    {
+        private readonly Configuration configuration;
+
+        public FileSystemService(Configuration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public IEnumerable<string> GetFilePaths()
+        {
+            return Directory.EnumerateFiles(configuration.FolderPath);
+        }
+
+        public string GenerateChecksum(string filePath)
+        {
+            try
+            {
+                using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    var sha = new SHA256Managed();
+                    var checksumBytes = sha.ComputeHash(stream);
+
+                    return BitConverter.ToString(checksumBytes).Replace("-", string.Empty);
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Error generating checksum: {0}", ex.Message);
+
+                return string.Empty;
+            }
+        }
+
+        public void DeleteFiles(IEnumerable<string> filesToDelete)
+        {
+            foreach (var fileToDelete in filesToDelete)
+            {
+                DeleteFile(fileToDelete);
+            }
+        }
+
+        public void DeleteFile(string fileToDelete)
+        {
+            File.Delete(fileToDelete);
+            Console.WriteLine("Deleted {0}", fileToDelete);
+        }
+    }
+}
